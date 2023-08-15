720ec419f52d9d2528b8d4638a1434ae6cac6357
Mark Faust <markfaust123@gmail.com>
2023-08-15 15:04:16 -0400

    Added requests for location usage and built getLocaiton() function.

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:clima/utilities/constants.dart';
-import 'package:geolocator/geolocator.dart';
 
 class LocationScreen extends StatefulWidget {
   @override
